1c24218964bc2b92a6f19207fb40869244311b43
RubenU2002 <rsantiagourrego@gmail.com>
2022-11-05 23:08:14 -0500

    Add

--- frontend/src/components/EventHead.jsx
+++ frontend/src/components/EventHead.jsx
@@ -3,9 +3,15 @@ import "../styles/eventHead.css"
 export default function EventHead({id}){
     const {name,city,date} = data.events.find(dato => dato.id === Number(id));
     return(
+        <>
         <div>
             <h3>{name}-{city}</h3>
             <li>{String(date)}</li>
+            
         </div>
+        <div>
+
+        </div>
+        </>
     )
 };
